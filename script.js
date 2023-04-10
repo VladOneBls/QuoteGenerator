aebb64b8f2aeff3c07ef2e4ec5d15ae6c370037e
Vlad Balas <balash_vlad@yahoo.com>
2023-04-10 13:23:42 +0100

    Small updates + favicon

--- script.js
+++ script.js
@@ -7,33 +7,31 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Show Loading
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+function removeLoadingSpinner() {
     quoteContainer.hidden = false;
     loader.hidden = true;
 }
 
 // Show New Quote
 function newQuote() {
-    loading();
+    showLoadingSpinner();
 
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]; // Math.random will return a random number between 0 and 1; multiplied by the number of quotes retrieved by the api so that will never run out; Math.floor will drop any decimals and return an integer
     
     // Check if Author field is blank and replace it with 'Unknown'
     if (!quote.author) {
-        authorText.textContent = 'Unknown';
+        authorText.textContent = '( Unknown )';
     } else {
-        authorText.textContent = quote.author;
+        authorText.textContent = `( ${quote.author} )`;
     }
 
-    // Check Quote length to determine styling
+    // Dynamically reduce font size for longer quotes
     if (quote.text.length > 120) {
         quoteText.classList.add('long-quote');
     } else {
@@ -41,26 +39,26 @@ function newQuote() {
     }
 
     // Set Quote, Hide Loader
-    quoteText.textContent = quote.text;
-    complete();
+    quoteText.textContent = `"${quote.text}"`;
+    removeLoadingSpinner();
 }
 
 // Get Quotes from API
 async function getQuotes() {
-    loading();
+    showLoadingSpinner();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
-        // Catch Error Here
+        console.log('Whoops, no quote', error);
     }
 }
 
 // Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text="${quoteText.textContent}"   ${authorText.textContent}`;
     window.open(twitterUrl, '_blank');
 }
 
@@ -70,13 +68,3 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
-
-
-
-// IF I WANT TO GET QUOTES FROM quotes.js REPLACE EVERYTHING WITH:
-// function newQuote() {
-//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-//     console.log(quote);
-// }
-
-// newQuote();
